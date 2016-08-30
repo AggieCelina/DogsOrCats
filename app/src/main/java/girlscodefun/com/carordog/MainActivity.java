7cedc592275389cf261dc8190fe80cf8b0d8fc9a
Tomasz Chabinka <tomaszchabinka@gmail.com>
2016-08-30 22:30:59 +0200

    nowa funkcja do wyświetlania głosów na psy

--- app/src/main/java/girlscodefun/com/carordog/MainActivity.java
+++ app/src/main/java/girlscodefun/com/carordog/MainActivity.java
@@ -2,12 +2,31 @@ package girlscodefun.com.carordog;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    // ustawiamy wartość zmiennej typu int (liczby całkowite) na 0
+    int dogVotesCounter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void voteForDog(View view) {
+        // tworzymy zmienną dogVotes - odwołanie do TextView w naszym layout o id dog_votes
+        TextView dogVotes = (TextView) findViewById(R.id.dog_votes);
+
+        // dodajemy jeden do poprzedniej wartości zmiennej dogVotesCounter; do zmiennej przypisujemy nową wartość
+        dogVotesCounter = dogVotesCounter + 1;
+
+        // wyświetlamy ilość głosów na ekranie
+        dogVotes.setText( "" + dogVotesCounter );
+    }
+
+
 }
