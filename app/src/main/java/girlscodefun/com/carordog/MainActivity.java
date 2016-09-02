20f06013711a3bac53e4a60ab797c92b40bf53f1
AggieCelina <agnieszka.c.markiewicz@gmail.com>
2016-09-02 23:03:33 +0200

    Dodac do projektu mozliwosc oddawania glosow na koty

--- app/src/main/java/girlscodefun/com/carordog/MainActivity.java
+++ app/src/main/java/girlscodefun/com/carordog/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
 
     // ustawiamy wartość zmiennej typu int (liczby całkowite) na 0
     int dogVotesCounter = 0;
+    int catVotesCounter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +29,19 @@ public class MainActivity extends AppCompatActivity {
         dogVotes.setText( "" + dogVotesCounter );
     }
 
+    public void voteForCat(View view){
+
+        //zmienna catVotes to odwołanie do TextView o id cat_votes (patrz np android:id="@+id/cat_votes" w activity_main.xml)
+        TextView catVotes = (TextView) findViewById(R.id.cat_votes);
+
+        //za każdym wywołaniem metody (to znaczy kliknięciem buttona) dodaj 1 do zmiennej catVotesCounter
+        catVotesCounter = catVotesCounter +1;
+
+        //przypisz zmiennej catVotes tekst o treści równej wartości liczbowej catVotesCounter
+        catVotes.setText("" + catVotesCounter);
+
+        //wywołanie metody następuje po dodaniu atrybutu buttona android:onClick="nazwa metody" w pliku activity_main.xml
+
+    }
 
 }
